5a598bb0732d94ada8ed0e9c30c142fc3992786e
Andrew Mackay <andrewmackayd@gmail.com>
2021-11-22 14:51:07 +0000

    10th commit main

--- src/main/java/com/codeclan/example/pirate_service/models/Raid.java
+++ src/main/java/com/codeclan/example/pirate_service/models/Raid.java
@@ -1,14 +1,41 @@
 
 package com.codeclan.example.pirate_service.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "raids")
 public class Raid {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "location")
     private String location;
+    @Column( name = "loot")
     private int loot;
+
+    @ManyToMany
+    @JsonIgnoreProperties({"raids"})
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+            name = "pirates_raids",
+            joinColumns = { @JoinColumn(
+                    name = "raid_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "pirate_id",
+                    nullable = false,
+                    updatable = false)
+            })
     private List<Pirate>pirates;
 
     public Raid(String location, int loot) {
