feb3b3808b0438d516276675b48af9b341c3521c
Andrew Mackay <andrewmackayd@gmail.com>
2021-11-22 10:32:34 +0000

    AM, 1st commit main

---
+++ src/test/java/com/codeclan/example/pirate_service/PirateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.pirate_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PirateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
